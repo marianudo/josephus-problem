d28af65bb0f981b231cd865372243affbab42867
Mariano Navas <marianudo@gmail.com>
2016-11-16 20:18:18 +0100

    Fixes for the step longer than the circle use case
    When the step value is bigger than the circle size we have to start
    counting again from the beginning, because it is a circle, right? Well,
    until this commit that didn't work properly; and the result was
    incorrect. Also the tests were incorrect. This commit fixes all that.

--- src/main/scala/me/marianonavas/josephus/Circle.scala
+++ src/main/scala/me/marianonavas/josephus/Circle.scala
@@ -3,21 +3,47 @@ package me.marianonavas.josephus
 /**
   *
   */
-case class Circle[T](elementsInCircle: Seq[T]) {
+case class Circle[T](elementsInCircle: List[T]) {
+
+    private def makeCircleForElements(elements: List[T]): Stream[T] =
+        elements.foldLeft(Stream(): Stream[T]) {
+            (acc, value) => value #:: acc
+        }.reverse #::: makeCircleForElements(elements)
 
     lazy val stream: Stream[T] =
-        elementsInCircle.toStream.foldLeft(Stream(): Stream[T]) {
-            (acc, value) => value #:: acc
-        }.reverse #::: stream
+        makeCircleForElements(elementsInCircle)
 
     def removeNthPositionFromCircle(position: Int): Option[Circle[T]] = {
-        val maybeNthPosition: Option[T] = stream.drop(position - 1).headOption
-        val elementsInNewCircle = maybeNthPosition.map(value => elementsInCircle.filterNot(_ == value))
-        elementsInNewCircle.flatMap(l => if(l.isEmpty) None else Some(Circle(l)))
+        require(position > 0, "The position to remove from the circle must be greater than 0")
+
+        val before = stream.take(position - 1).toList
+
+        val valueToRemove = stream.take(position).last
+        val nextElementsStream = stream.drop(position)
+
+        val initialElementsSize = elementsInCircle.size
+
+        val after = nextElementsStream.take(initialElementsSize - (position - initialElementsSize % position)).toList
+
+        val resultingStream = after ++ before.filterNot(_ == valueToRemove).filterNot(after.contains).toSet
+
+        if(resultingStream.isEmpty) None
+        else Some(Circle(resultingStream))
+
+//        val before = stream.take(position - 1)
+//        val valueToRemove = stream.take(position).head
+//        val elementsToTake = elementsInCircle.size - position
+//        val stream1 = stream.drop(position)
+//        val after = stream1.take(elementsToTake).filterNot(_ == valueToRemove)
+//
+//        val resultingElements: Seq[T] = after ++ before
+//
+//        if (resultingElements.isEmpty) None
+//        else Some(Circle(resultingElements.toList))
     }
 }
 
 object Circle {
     def withIntElements(numOfElements: Int): Circle[Int] =
-        Circle(1 to numOfElements)
+        Circle((1 to numOfElements).toList)
 }
--- src/test/scala/me/marianonavas/josephus/CircleSpec.scala
+++ src/test/scala/me/marianonavas/josephus/CircleSpec.scala
@@ -15,15 +15,12 @@ class CircleSpec extends FlatSpec {
         assert(Circle.withIntElements(numElementsInCircle).stream.take(elemsToTake).toList == expectedSequence)
     }
 
-    private def dropNthPosition(numElementsInCircle: Int)(position: Int): Option[Circle[Int]] =
-        Circle.withIntElements(numElementsInCircle).removeNthPositionFromCircle(position)
-
-    private def dropNthPositionAndTake
-    (numElementsInCircle: Int)(position: Int, elementsToTake: Int): Option[Seq[Int]] = {
-        dropNthPosition(numElementsInCircle)(position).map(_.stream.take(elementsToTake))
+    private def dropNthPosition(numElementsInCircle: Int)(position: Int): Option[Stream[Int]] = {
+        val maybeResultingCircle = Circle.withIntElements(numElementsInCircle).removeNthPositionFromCircle(position)
+        maybeResultingCircle.map(_.stream)
     }
 
-    private val dropPositionFromCircleOf5AndTake = dropNthPositionAndTake(5)_
+    private val dropFrom5ElementsCircle = dropNthPosition(5)(_)
 
     "Taking 3 of 3 elements" should "return the sequence 1, 2, 3" in {
         takeTestWith3Elements(3, Seq(1, 2, 3))
@@ -53,16 +50,18 @@ class CircleSpec extends FlatSpec {
         takeTestWith5Elements(10, Seq(1, 2, 3, 4, 5, 1, 2, 3, 4, 5))
     }
 
-    "Removing the 3th position in a 5 elements circle and taking 7" should "return the sequence 1, 2, 4, 5, 1, 2, 4" in {
-        assert(dropPositionFromCircleOf5AndTake(3, 7).contains(Seq(1, 2, 4, 5, 1, 2, 4)))
+    "Removing the 3th position in a 5 elements circle" should "return the sequence 4, 5, 1, 2" in {
+        val maybeStream = dropFrom5ElementsCircle(3)
+        val maybeInts = maybeStream.map(_.take(4))
+        assert(maybeInts.contains(Seq(4, 5, 1, 2)))
     }
 
-    "Removing the 7th position in a 5 elements circle and taking 10" should "return the sequence 1, 3, 4, 5, 1, 3, 4, 5, 1, 3" in {
-        assert(dropPositionFromCircleOf5AndTake(7, 10).contains(Seq(1, 3, 4, 5, 1, 3, 4, 5, 1, 3)))
+    "Removing the 7th position in a 5 elements circle" should "return the sequence 3, 4, 5, 1" in {
+        assert(dropFrom5ElementsCircle(7).map(_.take(4)).contains(Seq(3, 4, 5, 1)))
     }
 
     "Removing the only position from a 1 element circle" should "return None" in {
-        val circle = Circle(Seq(1))
+        val circle = Circle(List(1))
         val maybeCircle = circle.removeNthPositionFromCircle(1)
 
         assert(maybeCircle.isEmpty)
