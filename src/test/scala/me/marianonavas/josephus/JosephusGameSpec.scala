1f04a5e2e043fa27ea8a482add95f8cbc938cbb3
Mariano Navas <marianudo@gmail.com>
2016-11-16 23:21:35 +0100

    Complete tests

--- src/test/scala/me/marianonavas/josephus/JosephusGameSpec.scala
+++ src/test/scala/me/marianonavas/josephus/JosephusGameSpec.scala
@@ -8,22 +8,6 @@ import org.scalatest.FlatSpec
 class JosephusGameSpec extends FlatSpec {
     import JosephusGame.playGame
 
-    "In a 5 members circle with a step of 2" should "win the 3th" in {
-        assert(playGame(5, 2) == 3)
-    }
-
-    "In a 5 members circle with a step of 3" should "win the 4th" in {
-        assert(playGame(5, 3) == 4)
-    }
-
-    "In a 5 members circle with a step of 1" should "win the 5th" in {
-        assert(playGame(5, 1) == 5)
-    }
-
-    "In a 5 members circle with a step of 7" should "win the 4th" in {
-        assert(playGame(5, 7) == 4)
-    }
-
     // Here let's test the cases that the assignment tell us to test
     // What happens if n = k?
     "In a 5 members circle with a step of 5" should "win the 2th" in {
@@ -42,7 +26,29 @@ class JosephusGameSpec extends FlatSpec {
     // What happens if n is very large, but k = 2?
     // Not really feeling like doing this manually, so we assume that the calculation is correct
     // It terminates at least
-    "In a 500 members circle with a step of 2" should "win the " in {
+    "In a 100 members circle with a step of 2" should "win the 73" in {
+        assert(playGame(100, 2) == 73)
+    }
+
+    "In a 500 members circle with a step of 2" should "win the 489th" in {
         assert(playGame(500, 2) == 489)
+
+    }
+
+    // Now some more
+    "In a 5 members circle with a step of 2" should "win the 3th" in {
+        assert(playGame(5, 2) == 3)
+    }
+
+    "In a 5 members circle with a step of 3" should "win the 4th" in {
+        assert(playGame(5, 3) == 4)
+    }
+
+    "In a 5 members circle with a step of 1" should "win the 5th" in {
+        assert(playGame(5, 1) == 5)
+    }
+
+    "In a 5 members circle with a step of 7" should "win the 4th" in {
+        assert(playGame(5, 7) == 4)
     }
 }
